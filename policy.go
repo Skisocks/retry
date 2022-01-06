b042c01e7c7c064cbef3f4e03bc2ab003dbbf98e
archieskeoch <jaa.skeoch@gmail.com>
2022-01-06 23:29:09 +0000

    feat: add default backoff policy

--- policy.go
+++ policy.go
@@ -1,7 +1,7 @@
 package retry
 
 /*
-Policy contains the back off parameters that this package implements.
+BackoffPolicy contains the back off parameters that this package implements.
 
 MaxRetries: The maximum number of attempts that Retry will make.
 MaxBackoff: The maximum amount of time in milliseconds that Retry will backoff for.
@@ -9,7 +9,7 @@ BackoffMultiplier: The multiplier used to increase the backoff delay exponential
 MaxRandomJitter: The maximum value of random jitter added to the delay in milliseconds. 0 is no jitter.
 InitialDelay: The initial delay in milliseconds/
 */
-type Policy struct {
+type BackoffPolicy struct {
 	MaxRetries        int   // Maximum number of attempts
 	MaxBackoff        int   // Maximum backoff time in milliseconds. 0 is no maximum backoff
 	BackoffMultiplier int32 // Multiplier added to delay between attempts
@@ -17,15 +17,33 @@ type Policy struct {
 	InitialDelay      int32 // Delay in milliseconds
 }
 
-// NewPolicy returns a backoff Policy
-func NewPolicy(
+const (
+	DefaultMaxRetries        int   = 5
+	DefaultMaxBackoff        int   = 0
+	DefaultBackoffMultiplier int32 = 2
+	DefaultMaxRandomJitter   int32 = 1000
+	DefaultInitialDelay      int32 = 1000
+)
+
+// NewBackoffPolicy returns a BackoffPolicy with default parameters
+func NewBackoffPolicy() *BackoffPolicy {
+	return &BackoffPolicy{
+		MaxRetries:        DefaultMaxRetries,
+		MaxBackoff:        DefaultMaxBackoff,
+		BackoffMultiplier: DefaultBackoffMultiplier,
+		MaxRandomJitter:   DefaultMaxRandomJitter,
+		InitialDelay:      DefaultInitialDelay,
+	}
+}
+
+func NewCustomBackoffPolicy(
 	maxRetries int,
 	maxBackoff int,
 	backoffMultiplier int32,
 	maxRandomJitter int32,
 	initialDelay int32,
-) *Policy {
-	return &Policy{
+) *BackoffPolicy {
+	return &BackoffPolicy{
 		MaxRetries:        maxRetries,
 		MaxBackoff:        maxBackoff,
 		BackoffMultiplier: backoffMultiplier,
