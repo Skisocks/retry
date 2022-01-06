9498233ef6b6988ca9820dcd0ab61d3f4848cecf
archieskeoch <jaa.skeoch@gmail.com>
2022-01-06 23:38:01 +0000

    docs: fix comments

--- policy.go
+++ policy.go
@@ -1,28 +1,20 @@
 package retry
 
-/*
-BackoffPolicy contains the back off parameters that this package implements.
-
-MaxRetries: The maximum number of attempts that Retry will make.
-MaxBackoff: The maximum amount of time in milliseconds that Retry will backoff for.
-BackoffMultiplier: The multiplier used to increase the backoff delay exponentially.
-MaxRandomJitter: The maximum value of random jitter added to the delay in milliseconds. 0 is no jitter.
-InitialDelay: The initial delay in milliseconds/
-*/
+// BackoffPolicy contains the back off parameters that this package implements.
 type BackoffPolicy struct {
-	MaxRetries        int   // Maximum number of attempts
-	MaxBackoff        int   // Maximum backoff time in milliseconds. 0 is no maximum backoff
-	BackoffMultiplier int32 // Multiplier added to delay between attempts
+	MaxRetries        int   // Maximum number of attempts that Retry will make.
+	MaxBackoff        int   // Maximum amount of time in milliseconds that Retry will backoff for.
+	BackoffMultiplier int32 // Multiplier used to increase the backoff delay exponentially.
 	MaxRandomJitter   int32 // Maximum value of random jitter added to the delay in milliseconds. 0 is no jitter.
-	InitialDelay      int32 // Delay in milliseconds
+	InitialDelay      int32 // Initial delay in milliseconds
 }
 
 const (
-	DefaultMaxRetries        int   = 5
+	DefaultMaxRetries        int   = 0
 	DefaultMaxBackoff        int   = 0
 	DefaultBackoffMultiplier int32 = 2
 	DefaultMaxRandomJitter   int32 = 1000
-	DefaultInitialDelay      int32 = 1000
+	DefaultInitialDelay      int32 = 500
 )
 
 // NewBackoffPolicy returns a BackoffPolicy with default parameters
